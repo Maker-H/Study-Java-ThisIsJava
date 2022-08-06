eee6f08e0141ea4ec670c218dbabee3857772c90
Heesom <heesom.hs@gmail.com>
2022-08-06 20:51:28 +0900

    Chapter4_0

---
+++ Chapter1-Chapter5/DoWhileExample.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class DoWhileExample{
+    public static void main(String[] args) {
+      System.out.println("enter message");
+      System.out.println("If you want to end program enter q");
+
+      Scanner scanner = new Scanner(System.in);
+      String inputString;
+
+      do {
+        System.out.print(">");
+        inputString = scanner.nextLine();
+        System.out.println(inputString);
+      }while(!inputString.equals("q"));//end of do-while
+
+      System.out.println();
+      System.out.println("end of program");
+      
+    } //end of main
+
+
+    
+}//end of class
