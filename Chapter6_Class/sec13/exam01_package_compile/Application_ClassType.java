876416bcc20375a8a1c84fc0b7a2e9e05f6b7043
Heesom <heesom.hs@gmail.com>
2022-08-09 23:13:44 +0900

    Chapter6_6

--- Chapter6_Class/sec13/exam01_package_compile/Application_ClassType.java
+++ Chapter6_Class/sec13/exam01_package_compile/Application_ClassType.java
@@ -1,6 +1,6 @@
 package sec13.exam01_package_compile;
 
-public class asec13_Application_ClassType {
+public class Application_ClassType {
     public static void main(String[] args) {
         System.out.println("start application");
     }
